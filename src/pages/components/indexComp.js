d897cbca7a13cda62a4ed40bb36c6ad0fd80c13a
mczernyk <mczernyk@gmail.com>
2020-06-04 04:49:15 -0400

    troubleshooting

--- src/pages/components/indexComp.js
+++ src/pages/components/indexComp.js
@@ -1,69 +1,70 @@
-export { default as Intro } from "./Intro"
-export { default as Projects } from "./Projects"
-export { default as ProjectsBoxes } from "./ProjectsBoxes"
-export { default as PopupCrum } from "./PopupCrum"
-export { default as PopupMex } from "./PopupMex"
-export { default as PopupFriend } from "./PopupFriend"
-export { default as PopupGame } from "./PopupGame"
-export { default as PopupCoffee } from "./PopupCoffee"
-export { default as Experience } from "./Experience"
-export { default as Education } from "./Education"
-export { default as Design } from "./Design"
-export { default as DesignBoxes } from "./DesignBoxes"
-export { default as DesignGraphic } from "./DesignGraphic"
-export { default as DesignDigital } from "./DesignDigital"
-export { default as DesignArt } from "./DesignArt"
-export { default as CarouselDesign } from "./CarouselDesign"
-export { default as CarouselDigital } from "./CarouselDigital"
-export { default as CarouselArt } from "./CarouselArt"
-export { default as Music } from "./Music"
-export { default as Info } from "./Info"
-export { default as Sidebar } from "./Sidebar"
+// export { default as Intro } from "./Intro"
+// export { default as Projects } from "./Projects"
+// export { default as ProjectsBoxes } from "./ProjectsBoxes"
+// export { default as PopupCrum } from "./PopupCrum"
+// export { default as PopupMex } from "./PopupMex"
+// export { default as PopupFriend } from "./PopupFriend"
+// export { default as PopupGame } from "./PopupGame"
+// export { default as PopupCoffee } from "./PopupCoffee"
+// export { default as Experience } from "./Experience"
+// export { default as Education } from "./Education"
+// export { default as Design } from "./Design"
+// export { default as DesignBoxes } from "./DesignBoxes"
+// export { default as DesignGraphic } from "./DesignGraphic"
+// export { default as DesignDigital } from "./DesignDigital"
+// export { default as DesignArt } from "./DesignArt"
+// export { default as CarouselDesign } from "./CarouselDesign"
+// export { default as CarouselDigital } from "./CarouselDigital"
+// export { default as CarouselArt } from "./CarouselArt"
+// export { default as Music } from "./Music"
+// export { default as Info } from "./Info"
+// export { default as Sidebar } from "./Sidebar"
 
-// import Intro from "./Intro"
-// import Projects from "./Projects"
-// import ProjectsBoxes from "./ProjectsBoxes"
-// import PopupCrum from "./PopupCrum"
-// import PopupMex from "./PopupMex"
-// import PopupFriend from "./PopupFriend"
-// import PopupGame from "./PopupGame"
-// import PopupCoffee from "./PopupCoffee"
-// import Experience from "./Experience"
-// import Education from "./Education"
-// import Design from "./Design"
-// import DesignBoxes from "./DesignBoxes"
-// import DesignGraphic from "./DesignGraphic"
-// import DesignDigital from "./DesignDigital"
-// import DesignArt from "./DesignArt"
-// import CarouselDesign from "./CarouselDesign"
-// import CarouselDigital from "./CarouselDigital"
-// import CarouselArt from "./CarouselArt"
-// import Music from "./Music"
-// import Info from "./Info"
-// import Sidebar from "./Sidebar"
+import Intro from "./Intro"
+import Projects from "./Projects"
+import ProjectsBoxes from "./ProjectsBoxes"
+import PopupCrum from "./PopupCrum"
+import PopupMex from "./PopupMex"
+import PopupFriend from "./PopupFriend"
+import PopupGame from "./PopupGame"
+import PopupCoffee from "./PopupCoffee"
+import Experience from "./Experience"
+import Education from "./Education"
+import Design from "./Design"
+import DesignBoxes from "./DesignBoxes"
+import DesignGraphic from "./DesignGraphic"
+import DesignDigital from "./DesignDigital"
+import DesignArt from "./DesignArt"
+import CarouselDesign from "./CarouselDesign"
+import CarouselDigital from "./CarouselDigital"
+import CarouselArt from "./CarouselArt"
+import Music from "./Music"
+import Info from "./Info"
+import Sidebar from "./Sidebar"
+import {Scroll} from "./Scroll"
 
-// export {Intro};
-// export {Projects};
-// export {ProjectsBoxes};
-// export {PopupCrum};
-// export {PopupMex};
-// export {PopupFriend};
-// export {PopupGame};
-// export {PopupCoffee};
-// export {Experience};
-// export {Education};
-// export {Design};
-// export {DesignBoxes};
-// export {DesignGraphic};
-// export {DesignDigital};
-// export {DesignArt};
-// export {CarouselDesign};
-// export {CarouselDigital};
-// export {CarouselArt};
-// export {Music};
-// export {Info};
-// export {Sidebar};
-
+export {Intro};
+export {Projects};
+export {ProjectsBoxes};
+export {PopupCrum};
+export {PopupMex};
+export {PopupFriend};
+export {PopupGame};
+export {PopupCoffee};
+export {Experience};
+export {Education};
+export {Design};
+export {DesignBoxes};
+export {DesignGraphic};
+export {DesignDigital};
+export {DesignArt};
+export {CarouselDesign};
+export {CarouselDigital};
+export {CarouselArt};
+export {Music};
+export {Info};
+export {Sidebar};
+export default Scroll
 
 
 
